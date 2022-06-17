b752c7a91e96370ce328a97948cded4ffd497d47
REALcuba <angelskcuba@gmail.com>
2022-06-17 22:31:43 +0200

    sign in and login button done

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <button onClick={showLogin}>login</button>
+        </div>
+    )
+}
+
+const showLogin = () => {
+    <input type="text" />;
+    <input type="text" />;
+    alert("you click me");
+
+}
+export default Login
---
+++ src/components/Login/SignIn/SignIn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function SignIn() {
+    return (
+        <div>
+            <button onClick={showSignIn}>Sign In</button>
+        </div>
+    )
+}
+const showSignIn = () => {
+    <input type="text" />;
+    <input type="text" />;
+    alert("you click me");
+
+}
+export default SignIn
